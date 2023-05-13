836067d7c874290b632480cd75e18c63817cbac8
azuki774 <azuki774s@gmail.com>
2023-05-13 15:32:38 +0900

    wip dispatcher impl

--- internal/dispatcher/dispatcher.go
+++ internal/dispatcher/dispatcher.go
@@ -1,7 +1,30 @@
 package dispatcher
 
-import "batchdispatcher/internal/job"
+import (
+	"batchdispatcher/internal/job"
+	"context"
+	"fmt"
+)
 
 type Dispatcher struct {
 	Jobs []job.Job
+}
+
+func (d *Dispatcher) Run(ctx context.Context, jobname string) (err error) {
+	for _, j := range d.Jobs {
+		if j.Name == jobname {
+			// Job nameが一致する場合のみ実行
+			if j.GetStatus() == job.StatusRunning {
+				// already running
+				return fmt.Errorf("already running")
+			}
+
+			// job Run
+			go j.Run(ctx)
+			return nil
+		}
+	}
+
+	// job not found
+	return fmt.Errorf("job not found")
 }
--- internal/job/job.go
+++ internal/job/job.go
@@ -1,6 +1,9 @@
 package job
 
 import (
+	"batchdispatcher/internal/timeutil"
+	"context"
+	"os/exec"
 	"time"
 )
 
@@ -16,7 +19,7 @@ var (
 type Job struct {
 	Name             string
 	BatchDir         string
-	Status           JobStatus
+	status           JobStatus
 	LastChangeStatus time.Time
 }
 
@@ -24,7 +27,34 @@ func NewJob(name, batchDir string) Job {
 	return Job{
 		Name:             name,
 		BatchDir:         batchDir,
-		Status:           StatusNotRunning,
-		LastChangeStatus: time.Now(),
+		status:           StatusNotRunning,
+		LastChangeStatus: timeutil.NowFunc(),
 	}
+}
+
+func (j *Job) Run(ctx context.Context) {
+	j.ChangeStatus(StatusRunning)
+	cmd := exec.Command(j.BatchDir)
+	err := cmd.Run()
+
+	if err != nil {
+		j.ChangeStatus(StatusError)
+	}
+
+	exitCode := cmd.ProcessState.ExitCode()
+	if exitCode != 0 {
+		j.ChangeStatus(StatusError)
+
+	}
+
+	j.ChangeStatus(StatusComplete)
+}
+
+func (j *Job) ChangeStatus(nextStatus JobStatus) {
+	j.status = nextStatus
+	j.LastChangeStatus = timeutil.NowFunc()
+}
+
+func (j *Job) GetStatus() (status JobStatus) {
+	return j.status
 }
