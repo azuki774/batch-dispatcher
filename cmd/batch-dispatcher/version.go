e616bed2d2ca5723f1bfa11a45554f5f0f90d048
azuki774 <azuki774s@gmail.com>
2023-05-14 21:16:22 +0900

    Makefile version command

--- cmd/batch-dispatcher/version.go
+++ cmd/batch-dispatcher/version.go
@@ -17,7 +17,9 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("version called")
+		fmt.Printf("version: %s\n", version)
+		fmt.Printf("reversion: %s\n", revision)
+		fmt.Printf("build: %s\n", build)
 	},
 }
 
